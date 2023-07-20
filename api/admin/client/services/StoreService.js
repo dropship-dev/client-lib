a65721401941147b7ed2415d8982060dcf54b760
nvti <tiena2cva@gmail.com>
2023-07-20 16:57:15 +0000

    Update build to output generated at 2388e24

--- api/admin/client/services/StoreService.js
+++ api/admin/client/services/StoreService.js
@@ -10,7 +10,7 @@ class StoreService {
      * @returns any Ok
      * @throws ApiError
      */
-    getAllStores({ pageSize = 20, status, orderBy, order, periodFrom = '2023-01-01T00:00:00.000Z', periodTo = '2023-07-20T11:13:07.825Z', nextPageIndex, name, userId, revenueFrom, revenueTo, }) {
+    getAllStores({ pageSize = 20, status, orderBy, order, periodFrom = '2023-01-01T00:00:00.000Z', periodTo = '2023-07-20T16:56:27.846Z', nextPageIndex, name, userId, revenueFrom, revenueTo, }) {
         return this.httpRequest.request({
             method: 'GET',
             url: '/store',
