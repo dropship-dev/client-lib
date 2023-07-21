2eb4d7f8ab817a98bb0e9d9faaf1bc2278e768ff
dropship-dev <tiena2cva@gmail.com>
2023-07-21 09:06:13 +0000

    Update from https://github.com/dropship-dev/backend/commit/dc1ff680c7adda4a6b765609ce3ec2afede5e812

--- api/seller/client/services/StoreService.ts
+++ api/seller/client/services/StoreService.ts
@@ -65,7 +65,7 @@ export class StoreService {
     orderBy,
     order,
     periodFrom = '2023-01-01T00:00:00.000Z',
-    periodTo = '2023-07-21T08:59:23.624Z',
+    periodTo = '2023-07-21T09:05:49.359Z',
     nextPageIndex,
     name,
     userId,
