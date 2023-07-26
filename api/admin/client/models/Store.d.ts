0a64a3b87a8716c4aecf8dfa41b33660d41bdc80
nvti <tiena2cva@gmail.com>
2023-07-26 13:34:11 +0000

    Update build to output generated at 9e7f6a7

--- api/admin/client/models/Store.d.ts
+++ api/admin/client/models/Store.d.ts
@@ -18,6 +18,7 @@ export type Store = {
     customDomainStatus: CustomDomainStatus;
     customDomain: string | null;
     domain: string;
+    pageName: string;
     email: string;
     address: string;
     phone: string;
