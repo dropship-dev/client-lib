6804aa1600f4d663070c714d003c9836cb5f3486
nvti <tiena2cva@gmail.com>
2023-07-31 08:15:20 +0000

    Update build to output generated at 4a9ac0e

--- api/admin/client/models/CreateOrderDto.d.ts
+++ api/admin/client/models/CreateOrderDto.d.ts
@@ -2,15 +2,15 @@ import type { EmailDto } from './EmailDto';
 import type { NameDto } from './NameDto';
 import type { PhoneWithCountryCode } from './PhoneWithCountryCode';
 export type CreateOrderDto = {
-    name: NameDto;
-    email: EmailDto;
+    name?: NameDto;
+    email?: EmailDto;
     phone?: PhoneWithCountryCode;
-    address1: string;
-    address2: string;
-    city: string;
-    province: string;
-    zipCode: string;
-    country: string;
+    address1?: string;
+    address2?: string;
+    city?: string;
+    province?: string;
+    zipCode?: string;
+    country?: string;
     note?: string;
     payment: {
         method: CreateOrderDto.method;
