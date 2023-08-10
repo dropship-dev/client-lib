91ecf881ef6a77dc60c452ad78df213e3d5f19d0
dropship-dev <tiena2cva@gmail.com>
2023-08-10 06:22:04 +0000

    Update from https://github.com/dropship-dev/backend/commit/d7cb9f7a1e12218b16dc9de1435aba077ff964f3

--- api/admin/client/services/PerformanceService.ts
+++ api/admin/client/services/PerformanceService.ts
@@ -20,7 +20,7 @@ export class PerformanceService {
    */
   public getRevenueOverTime({
     startDate = '2023-01-01T00:00:00.000Z',
-    endDate = '2023-08-10T04:19:12.970Z',
+    endDate,
     storeId,
   }: {
     startDate?: string,
@@ -54,7 +54,7 @@ export class PerformanceService {
    */
   public getCrOverTime({
     startDate = '2023-01-01T00:00:00.000Z',
-    endDate = '2023-08-10T04:19:12.993Z',
+    endDate,
     storeId,
   }: {
     startDate?: string,
@@ -94,7 +94,7 @@ export class PerformanceService {
    */
   public getTopProductsByOrders({
     startDate = '2023-01-01T00:00:00.000Z',
-    endDate = '2023-08-10T04:19:13.009Z',
+    endDate,
     storeId,
     limit = 10,
   }: {
@@ -128,7 +128,7 @@ export class PerformanceService {
    */
   public getTopStoresByRevenue({
     startDate = '2023-01-01T00:00:00.000Z',
-    endDate = '2023-08-10T04:19:13.012Z',
+    endDate,
     limit = 10,
   }: {
     startDate?: string,
@@ -159,7 +159,7 @@ export class PerformanceService {
    */
   public getPerformanceSummary({
     startDate = '2023-01-01T00:00:00.000Z',
-    endDate = '2023-08-10T04:19:13.029Z',
+    endDate,
     storeId,
   }: {
     startDate?: string,
@@ -207,7 +207,7 @@ export class PerformanceService {
    */
   public getProductPerformance({
     startDate = '2023-01-01T00:00:00.000Z',
-    endDate = '2023-08-10T04:19:13.034Z',
+    endDate,
     storeId,
     pageSize = 20,
     nextPageIndex,
--- api/admin/client/services/StoreService.ts
+++ api/admin/client/services/StoreService.ts
@@ -29,7 +29,7 @@ export class StoreService {
     orderBy,
     order,
     periodFrom = '2023-01-01T00:00:00.000Z',
-    periodTo = '2023-08-10T04:19:13.628Z',
+    periodTo,
     nextPageIndex,
     name,
     userId,
