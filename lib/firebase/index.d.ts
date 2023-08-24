139a909abd4bc3131dbc55a85cc376abd18bb786
nvti <tiena2cva@gmail.com>
2023-08-24 02:13:10 +0000

    Update build to output generated at edabba5

--- lib/firebase/index.d.ts
+++ lib/firebase/index.d.ts
@@ -3,4 +3,6 @@ export declare function getToken(): Promise<string>;
 export declare function isLoggedIn(): boolean;
 export declare function getUser(): import("@firebase/auth").User | null;
 export declare function signInWithGoogle(): Promise<import("@firebase/auth").User>;
+export declare function signInWithEmailPassword(email: string, password: string): Promise<import("@firebase/auth").User>;
+export declare function signUpWithEmailPassword(email: string, password: string): Promise<import("@firebase/auth").User>;
 export declare function signOut(): Promise<void>;
