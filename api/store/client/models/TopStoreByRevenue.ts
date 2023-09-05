496b0516f7f010ca260d91511fd70b0631323307
dropship-dev <tiena2cva@gmail.com>
2023-09-05 08:39:50 +0000

    Update from https://github.com/dropship-dev/backend/commit/689504a2f57ff85427ec59085ce1cda6eb690a54

--- api/store/client/models/TopStoreByRevenue.ts
+++ api/store/client/models/TopStoreByRevenue.ts
@@ -3,8 +3,14 @@
 /* tslint:disable */
 /* eslint-disable */
 
+import type { StoreStatus } from './StoreStatus';
+
 export type TopStoreByRevenue = {
+  status: StoreStatus;
   revenue: number;
+  phone: string;
+  email: string;
+  primaryDomain: string;
   name: string;
   storeId: string;
 };
