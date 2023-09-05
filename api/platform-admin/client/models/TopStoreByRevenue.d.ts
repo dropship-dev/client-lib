90f2aac192c1ddd693190ffa8398c177cf318bd8
nvti <tiena2cva@gmail.com>
2023-09-05 08:40:23 +0000

    Update build to output generated at 496b051

--- api/platform-admin/client/models/TopStoreByRevenue.d.ts
+++ api/platform-admin/client/models/TopStoreByRevenue.d.ts
@@ -1,5 +1,10 @@
+import type { StoreStatus } from './StoreStatus';
 export type TopStoreByRevenue = {
+    status: StoreStatus;
     revenue: number;
+    phone: string;
+    email: string;
+    primaryDomain: string;
     name: string;
     storeId: string;
 };
