e73c8a556e9dec471989bd0ce92a7de933e38710
dropship-dev <tiena2cva@gmail.com>
2023-09-12 09:31:24 +0000

    Update from https://github.com/dropship-dev/backend/commit/90ca3436e73f57839a02c9ab5e74372c49fa728b

---
+++ api/admin/client/models/StoreProductPerformanceResp.ts
@@ -0,0 +1,17 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type StoreProductPerformanceResp = {
+  id: string;
+  name: string;
+  primaryDomain: string;
+  products: Array<{
+    revenue: number;
+    photo: string;
+    name: string;
+    id: string;
+  }>;
+};
+
