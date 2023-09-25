7a07563ca93931e20ab1f7145e7587e2d6177fd4
dropship-dev <tiena2cva@gmail.com>
2023-09-25 10:19:14 +0000

    Update from https://github.com/dropship-dev/backend/commit/2b211590f2ffac253a510e12b869af6a85cc7c7f

--- api/admin/client/models/OrderItem.ts
+++ api/admin/client/models/OrderItem.ts
@@ -15,6 +15,7 @@ export type OrderItem = {
   currencyId: number;
   orderId: string;
   productVariantId: number | null;
+  productId: number;
   tracking: string | null;
   status: FulfillmentStatus;
   platformPrice: number;
