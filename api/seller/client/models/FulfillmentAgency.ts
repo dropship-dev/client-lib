c6971a7f8bffa8bce34670231f79ba1aabf8374b
dropship-dev <tiena2cva@gmail.com>
2023-09-28 09:45:44 +0000

    Update from https://github.com/dropship-dev/backend/commit/4cad04f0b9998c7bdc9f9bdf96cfe15b1f49e40b

---
+++ api/seller/client/models/FulfillmentAgency.ts
@@ -0,0 +1,15 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * Model FulfillmentAgency
+ */
+export type FulfillmentAgency = {
+  updatedAt: string;
+  createdAt: string;
+  name: string;
+  id: number;
+};
+
