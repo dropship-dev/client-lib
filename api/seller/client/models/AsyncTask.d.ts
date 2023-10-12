fe9b1a08e327f4f90fb23e5817df9f7b9bd98f29
nvti <tiena2cva@gmail.com>
2023-10-12 07:33:39 +0000

    Update build to output generated at 8251a06

--- api/seller/client/models/AsyncTask.d.ts
+++ api/seller/client/models/AsyncTask.d.ts
@@ -1,12 +1,14 @@
 import type { AsyncTaskStatus } from './AsyncTaskStatus';
 import type { AsyncTaskType } from './AsyncTaskType';
+import type { Prisma_JsonValue } from './Prisma_JsonValue';
 /**
  * Model AsyncTask
  */
 export type AsyncTask = {
     updatedAt: string;
     createdAt: string;
-    result: string | null;
+    result: Prisma_JsonValue | null;
+    input: Prisma_JsonValue | null;
     status: AsyncTaskStatus;
     type: AsyncTaskType;
     id: string;
