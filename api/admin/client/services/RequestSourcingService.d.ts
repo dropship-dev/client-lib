7c5dd7b0091316ece1fc22fad619b26a41d07839
nvti <tiena2cva@gmail.com>
2023-11-21 10:32:26 +0000

    Update build to output generated at 28c9512

--- api/admin/client/services/RequestSourcingService.d.ts
+++ api/admin/client/services/RequestSourcingService.d.ts
@@ -1,11 +1,34 @@
 import type { ApproveRequestSourcingDto } from '../models/ApproveRequestSourcingDto';
 import type { RejectRequestSourcingDto } from '../models/RejectRequestSourcingDto';
 import type { RequestSourcing } from '../models/RequestSourcing';
+import type { RequestSourcingStatus } from '../models/RequestSourcingStatus';
+import type { Store } from '../models/Store';
 import type { CancelablePromise } from '../core/CancelablePromise';
 import type { BaseHttpRequest } from '../core/BaseHttpRequest';
 export declare class RequestSourcingService {
     readonly httpRequest: BaseHttpRequest;
     constructor(httpRequest: BaseHttpRequest);
+    /**
+     * @returns any Ok
+     * @throws ApiError
+     */
+    getAllRequestSourcing({ storeId, statusRequest, search, startDate, endDate, pageSize, nextPageIndex, }: {
+        storeId: string;
+        statusRequest?: Array<RequestSourcingStatus>;
+        search?: string;
+        startDate?: string;
+        endDate?: string;
+        pageSize?: number;
+        nextPageIndex?: number;
+    }): CancelablePromise<{
+        orderBy: string;
+        nextPageIndex: number;
+        prePageIndex: number;
+        total: number;
+        data: Array<(RequestSourcing & {
+            Store: Store;
+        })>;
+    }>;
     /**
      * @returns RequestSourcing Ok
      * @throws ApiError
