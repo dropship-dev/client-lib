f7592f9533f1f8f9ee761d18404341aef9ec36a0
nvti <tiena2cva@gmail.com>
2023-11-22 10:29:23 +0000

    Update build to output generated at b24a264

--- api/seller/client/models/PrismaJson_RequestSourcingHistory.d.ts
+++ api/seller/client/models/PrismaJson_RequestSourcingHistory.d.ts
@@ -1,5 +1,6 @@
+import type { RequestSourcingStatus } from './RequestSourcingStatus';
 export type PrismaJson_RequestSourcingHistory = Array<{
     createAt?: string;
     value?: string;
-    label: string;
+    label: RequestSourcingStatus;
 }>;
