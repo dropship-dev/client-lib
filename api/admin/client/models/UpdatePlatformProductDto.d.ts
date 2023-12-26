f24bd152b0bb90a20dc3d552c5ed68365d20c0b2
nvti <tiena2cva@gmail.com>
2023-12-26 08:14:29 +0000

    Update build to output generated at 7b64d8f

--- api/admin/client/models/UpdatePlatformProductDto.d.ts
+++ api/admin/client/models/UpdatePlatformProductDto.d.ts
@@ -14,4 +14,8 @@ export type UpdatePlatformProductDto = {
     variantOption?: PrismaJson_VariantOptions;
     PlatformVariant?: Array<UpdatePlatformProductVariantDto>;
     availableSet?: PrismaJson_AvailableSet;
+    GroupPlatformVariant?: Array<{
+        variants: Array<number>;
+        name: NameDto;
+    }>;
 };
