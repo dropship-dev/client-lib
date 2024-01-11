644c430ccb92b98dce32aec822a9e5eb0172f3e6
nvti <tiena2cva@gmail.com>
2024-01-11 03:19:26 +0000

    Update build to output generated at 30a2b98

--- api/platform-admin/client/models/Wallet.d.ts
+++ api/platform-admin/client/models/Wallet.d.ts
@@ -7,6 +7,7 @@ export type Wallet = {
     isDeleted: boolean;
     storeId: string | null;
     fulfillmentAgencyId: number | null;
+    balanceAvailable: number;
     balanceAmount: number;
     walletName: string | null;
     id: string;
