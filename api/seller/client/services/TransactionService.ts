e0777f7bdbc2b91b5ac7c37d6e8885774ac876da
dropship-dev <tiena2cva@gmail.com>
2024-01-11 10:16:06 +0000

    Update from https://github.com/dropship-dev/backend/commit/54141a05869b8c06cc4ef6291f513c72d64bd9dc

--- api/seller/client/services/TransactionService.ts
+++ api/seller/client/services/TransactionService.ts
@@ -3,7 +3,6 @@
 /* tslint:disable */
 /* eslint-disable */
 import type { Transaction } from '../models/Transaction';
-import type { TransactionType } from '../models/TransactionType';
 
 import type { CancelablePromise } from '../core/CancelablePromise';
 import type { BaseHttpRequest } from '../core/BaseHttpRequest';
@@ -18,29 +17,36 @@ export class TransactionService {
    */
   public getAllStoreTransaction({
     storeId,
-    transactionType,
+    fulfillmentAgencyId,
+    search,
+    startDate,
+    endDate,
     pageSize = 20,
     nextPageIndex,
   }: {
-    storeId: string,
-    transactionType?: Array<TransactionType>,
+    storeId?: string,
+    fulfillmentAgencyId?: number,
+    search?: string,
+    startDate?: string,
+    endDate?: string,
     pageSize?: number,
-    nextPageIndex?: number,
+    nextPageIndex?: string,
   }): CancelablePromise<{
     orderBy: string;
-    nextPageIndex: number;
-    prePageIndex: number;
+    nextPageIndex: string;
+    prePageIndex: string;
     total: number;
     data: Array<Transaction>;
   }> {
     return this.httpRequest.request({
       method: 'GET',
       url: '/store/{storeId}/transaction',
-      path: {
-        'storeId': storeId,
-      },
       query: {
-        'transactionType': transactionType,
+        'storeId': storeId,
+        'fulfillmentAgencyId': fulfillmentAgencyId,
+        'search': search,
+        'startDate': startDate,
+        'endDate': endDate,
         'pageSize': pageSize,
         'nextPageIndex': nextPageIndex,
       },
