30a2b988ff9bc6353ebb3e639c7178d49b1a3ec4
dropship-dev <tiena2cva@gmail.com>
2024-01-11 03:18:49 +0000

    Update from https://github.com/dropship-dev/backend/commit/187cf5d6a9d6fa213782cc9ac726f4ebb629c3f6

--- api/admin/client/models/Wallet.ts
+++ api/admin/client/models/Wallet.ts
@@ -12,6 +12,7 @@ export type Wallet = {
   isDeleted: boolean;
   storeId: string | null;
   fulfillmentAgencyId: number | null;
+  balanceAvailable: number;
   balanceAmount: number;
   walletName: string | null;
   id: string;
