e668fbb6ed8f4980cec7dad30c98f9c4a9cb76d5
dropship-dev <tiena2cva@gmail.com>
2024-03-13 08:48:16 +0000

    Update from https://github.com/dropship-dev/backend/commit/0299ffb8dbcdef6bfbee461cdbcbd05a1ffef493

---
+++ api/platform-admin/client/models/UpdateVariantsForSellerInDependenceDto.ts
@@ -0,0 +1,20 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { NameDto } from './NameDto';
+import type { PhotoDto } from './PhotoDto';
+import type { PrismaJson_VariantOptionValues } from './PrismaJson_VariantOptionValues';
+
+export type UpdateVariantsForSellerInDependenceDto = {
+  id: number;
+  name: NameDto;
+  photo: PhotoDto;
+  price: number;
+  supplierPrice: number;
+  compareAtPrice: number;
+  SKU: string;
+  variantOption: PrismaJson_VariantOptionValues;
+};
+
--- api/seller/client/models/UpdateProductDto.ts
+++ api/seller/client/models/UpdateProductDto.ts
@@ -8,7 +8,7 @@ import type { DiscountDto } from './DiscountDto';
 import type { NameDto } from './NameDto';
 import type { PhotoDto } from './PhotoDto';
 import type { PrismaJson_VariantOptions } from './PrismaJson_VariantOptions';
-import type { UpdateVariantsDto } from './UpdateVariantsDto';
+import type { UpdateVariantsForSellerInDependenceDto } from './UpdateVariantsForSellerInDependenceDto';
 
 export type UpdateProductDto = {
   name?: NameDto;
@@ -18,7 +18,7 @@ export type UpdateProductDto = {
   shippingFee?: number;
   shippingFeeAdditional?: number;
   variantOption?: PrismaJson_VariantOptions;
-  variants?: Array<UpdateVariantsDto>;
+  variants?: Array<UpdateVariantsForSellerInDependenceDto>;
   SKU?: string;
   permalink?: string;
   tags?: Array<string>;
