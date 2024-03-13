e9e4826162e26c84ba70d286e60a66ab51daca2a
nvti <tiena2cva@gmail.com>
2024-03-13 08:39:28 +0000

    Update build to output generated at 16512a7

--- api/seller/client/models/UpdateProductDto.d.ts
+++ api/seller/client/models/UpdateProductDto.d.ts
@@ -3,7 +3,7 @@ import type { DiscountDto } from './DiscountDto';
 import type { NameDto } from './NameDto';
 import type { PhotoDto } from './PhotoDto';
 import type { PrismaJson_VariantOptions } from './PrismaJson_VariantOptions';
-import type { UpdateVariantDto } from './UpdateVariantDto';
+import type { UpdateVariantsDto } from './UpdateVariantsDto';
 export type UpdateProductDto = {
     name?: NameDto;
     description?: string;
@@ -12,7 +12,7 @@ export type UpdateProductDto = {
     shippingFee?: number;
     shippingFeeAdditional?: number;
     variantOption?: PrismaJson_VariantOptions;
-    variant?: Array<UpdateVariantDto>;
+    variants?: Array<UpdateVariantsDto>;
     SKU?: string;
     permalink?: string;
     tags?: Array<string>;
