fe07e7e800e947435eaf8ae2a75ac54e0b03e763
nvti <tiena2cva@gmail.com>
2024-03-28 03:41:31 +0000

    Update build to output generated at 609c2e9

--- api/admin/client/models/UpdateOrderStatusDto.d.ts
+++ api/admin/client/models/UpdateOrderStatusDto.d.ts
@@ -1,9 +1,7 @@
 import type { FulfillmentStatus } from './FulfillmentStatus';
-import type { StoreType } from './StoreType';
 export type UpdateOrderStatusDto = {
     orders: Array<{
         orderItems: Array<{
-            type: StoreType;
             carrier?: string;
             tracking?: string;
             fulfillmentStatus?: FulfillmentStatus;
