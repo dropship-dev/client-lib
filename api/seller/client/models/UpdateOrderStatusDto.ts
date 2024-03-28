d3a4a769661d2f745c9d2a0099e4b6722053e7f6
dropship-dev <tiena2cva@gmail.com>
2024-03-28 02:46:36 +0000

    Update from https://github.com/dropship-dev/backend/commit/07d858bdcc81971e3ac1301b374199e3a72d995f

--- api/seller/client/models/UpdateOrderStatusDto.ts
+++ api/seller/client/models/UpdateOrderStatusDto.ts
@@ -4,10 +4,12 @@
 /* eslint-disable */
 
 import type { FulfillmentStatus } from './FulfillmentStatus';
+import type { StoreType } from './StoreType';
 
 export type UpdateOrderStatusDto = {
   orders: Array<{
     orderItems: Array<{
+      type: StoreType;
       carrier?: string;
       tracking?: string;
       fulfillmentStatus?: FulfillmentStatus;
