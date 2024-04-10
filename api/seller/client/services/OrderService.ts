71c828aeafb1de3b699ed353354d311f2c2b5df5
dropship-dev <tiena2cva@gmail.com>
2024-04-10 03:42:47 +0000

    Update from https://github.com/dropship-dev/backend/commit/0a7a77f24486108805113d830d72686d6ceaa430

--- api/seller/client/services/OrderService.ts
+++ api/seller/client/services/OrderService.ts
@@ -245,6 +245,7 @@ export class OrderService {
       mediaType: 'application/json',
       errors: {
         400: `Bad request`,
+        401: `Invalid token`,
         403: `Forbidden`,
         404: `Not found`,
         500: `Internal server error`,
