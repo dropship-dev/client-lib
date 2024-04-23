e862e58d76520e514e7f1a386dcf3406160e2735
dropship-dev <tiena2cva@gmail.com>
2024-04-23 05:01:32 +0000

    Update from https://github.com/dropship-dev/backend/commit/93d2b232274bd3904e498d09258842d1817ac230

--- api/platform-admin/client/models/Store.ts
+++ api/platform-admin/client/models/Store.ts
@@ -17,8 +17,6 @@ export type Store = {
   fulfillmentAgencyId: number | null;
   maxUsers: number;
   balance: number;
-  isSubscription: boolean | null;
-  subscriptionStoreId: string | null;
   referralCode: string | null;
   type: StoreType;
   defaultBankAccount: string | null;
