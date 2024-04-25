edbbe3f8253d61be009f6663c2721233eda8043a
nvti <tiena2cva@gmail.com>
2024-04-25 02:16:40 +0000

    Update build to output generated at 4995c02

--- api/admin/client/models/CreateVariantsDto.d.ts
+++ api/admin/client/models/CreateVariantsDto.d.ts
@@ -1,5 +1,6 @@
 import type { NameDto } from './NameDto';
 import type { PhotoDto } from './PhotoDto';
+import type { PrismaJson_VariantOptionValues } from './PrismaJson_VariantOptionValues';
 export type CreateVariantsDto = {
     name: NameDto;
     price: number;
@@ -8,4 +9,5 @@ export type CreateVariantsDto = {
     photo: PhotoDto;
     productId: number;
     supplierPrice: number;
+    variantOption?: PrismaJson_VariantOptionValues;
 };
