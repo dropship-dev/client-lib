9f7fd684302e26877d7beddb19c5ed2519db7bbf
dropship-dev <tiena2cva@gmail.com>
2024-06-13 06:30:35 +0000

    Update from https://github.com/dropship-dev/backend/commit/9df3b9d7f7bd570eba7544fcaf9614015e4d814e

--- api/admin/client/models/NameConditionDto.ts
+++ api/admin/client/models/NameConditionDto.ts
@@ -4,6 +4,7 @@
 /* eslint-disable */
 
 import type { CollectionField_name } from './CollectionField_name';
+import type { OperatorCollection_CONTAIN } from './OperatorCollection_CONTAIN';
 import type { OperatorCollection_ENDS_WITH } from './OperatorCollection_ENDS_WITH';
 import type { OperatorCollection_EQUAL_TO } from './OperatorCollection_EQUAL_TO';
 import type { OperatorCollection_NOT_CONTAIN } from './OperatorCollection_NOT_CONTAIN';
@@ -11,7 +12,7 @@ import type { OperatorCollection_STARTS_WITH } from './OperatorCollection_STARTS
 
 export type NameConditionDto = {
   field: CollectionField_name;
-  operator: (OperatorCollection_NOT_CONTAIN | OperatorCollection_EQUAL_TO | OperatorCollection_STARTS_WITH | OperatorCollection_ENDS_WITH);
+  operator: (OperatorCollection_NOT_CONTAIN | OperatorCollection_EQUAL_TO | OperatorCollection_STARTS_WITH | OperatorCollection_ENDS_WITH | OperatorCollection_CONTAIN);
   value: string;
 };
 
