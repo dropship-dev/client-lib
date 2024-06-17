4d4c9810052aea244deffa5cff8d9638119a556c
dropship-dev <tiena2cva@gmail.com>
2024-06-17 10:37:24 +0000

    Update from https://github.com/dropship-dev/backend/commit/a924aa8e1c5764d5173282b62989dc56eebedc21

--- api/seller/client/models/Theme.ts
+++ api/seller/client/models/Theme.ts
@@ -6,6 +6,7 @@
 import type { LogoSize } from './LogoSize';
 import type { Prisma_JsonValue } from './Prisma_JsonValue';
 import type { PrismaJson_Colors } from './PrismaJson_Colors';
+import type { PrismaJson_ThemeNodes } from './PrismaJson_ThemeNodes';
 import type { PrismaJson_ThemeSetting } from './PrismaJson_ThemeSetting';
 import type { PrismaJson_ThemeStyle } from './PrismaJson_ThemeStyle';
 
@@ -20,6 +21,10 @@ export type Theme = {
    */
   themeTemplateId: number | null;
   storeId: string;
+  /**
+   * [ThemeNodes]
+   */
+  nodes: PrismaJson_ThemeNodes | null;
   /**
    * [ThemeSetting]
    */
--- api/store/client/models/UpdateThemeDto.ts
+++ api/store/client/models/UpdateThemeDto.ts
@@ -7,12 +7,12 @@ import type { LogoSize } from './LogoSize';
 import type { NameDto } from './NameDto';
 import type { PhotoDto } from './PhotoDto';
 import type { PrismaJson_Colors } from './PrismaJson_Colors';
-import type { PrismaJson_ThemeNotes } from './PrismaJson_ThemeNotes';
+import type { PrismaJson_ThemeNodes } from './PrismaJson_ThemeNodes';
 import type { PrismaJson_ThemeSetting } from './PrismaJson_ThemeSetting';
 import type { PrismaJson_ThemeStyle } from './PrismaJson_ThemeStyle';
 
 export type UpdateThemeDto = {
-  commonNotes?: PrismaJson_ThemeNotes;
+  commonNodes?: PrismaJson_ThemeNodes;
   ThemePage?: Array<{
     content: string;
     id: number;
