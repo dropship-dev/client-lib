e682b47b9df255a5c1ce8b794536ee0aaad60552
dropship-dev <tiena2cva@gmail.com>
2024-06-18 04:15:22 +0000

    Update from https://github.com/dropship-dev/backend/commit/aee6079a0b82965e86297cbc4873b8f3d875c569

--- api/platform-admin/client/models/DiscountCrossSellType.ts
+++ api/platform-admin/client/models/DiscountCrossSellType.ts
@@ -3,8 +3,13 @@
 /* tslint:disable */
 /* eslint-disable */
 
+/**
+ * Just Percents amount will calculate base on subtotal
+ * the result calculate base on total
+ */
 export enum DiscountCrossSellType {
   PERCENT_ON_EACH_PRODUCT = 'Percent on each product',
   FIXED_AMOUNT_ON_EACH_PRODUCT = 'Fixed amount on each product',
   PERCENT_AMOUNT_ON_SUBTOTAL = 'Percent amount on subtotal',
+  DIRECT_AMOUNT_ON_PRODUCT = 'Direct amount on product',
 }
