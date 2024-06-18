a1a6c285d02c54d274c005dd36dce561c0e1a8a3
nvti <tiena2cva@gmail.com>
2024-06-18 04:15:43 +0000

    Update build to output generated at e682b47

--- api/admin/client/models/DiscountCrossSellType.d.ts
+++ api/admin/client/models/DiscountCrossSellType.d.ts
@@ -1,5 +1,10 @@
+/**
+ * Just Percents amount will calculate base on subtotal
+ * the result calculate base on total
+ */
 export declare enum DiscountCrossSellType {
     PERCENT_ON_EACH_PRODUCT = "Percent on each product",
     FIXED_AMOUNT_ON_EACH_PRODUCT = "Fixed amount on each product",
-    PERCENT_AMOUNT_ON_SUBTOTAL = "Percent amount on subtotal"
+    PERCENT_AMOUNT_ON_SUBTOTAL = "Percent amount on subtotal",
+    DIRECT_AMOUNT_ON_PRODUCT = "Direct amount on product"
 }
