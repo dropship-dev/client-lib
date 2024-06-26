39f9f801f5582772afb5423081e6bf6fad32906f
nvti <tiena2cva@gmail.com>
2024-06-26 09:21:46 +0000

    Update build to output generated at d57361e

--- api/seller/client/services/ProductService.d.ts
+++ api/seller/client/services/ProductService.d.ts
@@ -133,12 +133,7 @@ export declare class ProductService {
         };
         CrossSell: Array<(CrossSell & {
             Product: Array<(Product & {
-                ProductVariant: Array<{
-                    compareAtPrice: number;
-                    price: number;
-                    name: string;
-                    id: number;
-                }>;
+                ProductVariant: Array<ProductVariant>;
             })>;
             Collection: Array<Collection>;
         })>;
