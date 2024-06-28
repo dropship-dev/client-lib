1354622d2d1a382c44a31d4890badaa67c641025
nvti <tiena2cva@gmail.com>
2024-06-28 10:07:19 +0000

    Update build to output generated at 8194174

--- api/store/client/models/Store.d.ts
+++ api/store/client/models/Store.d.ts
@@ -11,6 +11,7 @@ export type Store = {
     fulfillmentAgencyId: number | null;
     maxUsers: number;
     balance: number;
+    isFraud: boolean | null;
     referralCode: string | null;
     type: StoreType;
     defaultBankAccount: string | null;
