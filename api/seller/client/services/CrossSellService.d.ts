1cd8cac3a1a43c67121dab3e32247eae7be05939
nvti <tiena2cva@gmail.com>
2024-07-04 09:57:29 +0000

    Update build to output generated at c3c7cdc

--- api/seller/client/services/CrossSellService.d.ts
+++ api/seller/client/services/CrossSellService.d.ts
@@ -38,7 +38,9 @@ export declare class CrossSellService {
         total: number;
         data: Array<(CrossSell & {
             Product: Array<Product>;
-            Collection: Array<Collection>;
+            Collection: Array<(Collection & {
+                Product: Array<Product>;
+            })>;
         })>;
     }>;
     /**
