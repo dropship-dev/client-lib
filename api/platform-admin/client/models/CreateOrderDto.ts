11bc723952af03589d19c4ae363cb47f2210db78
dropship-dev <tiena2cva@gmail.com>
2024-07-08 03:03:55 +0000

    Update from https://github.com/dropship-dev/backend/commit/3b77851718d8a4a1cc249b271a4f7eb30c9c8f99

--- api/platform-admin/client/models/CreateOrderDto.ts
+++ api/platform-admin/client/models/CreateOrderDto.ts
@@ -20,7 +20,7 @@ export type CreateOrderDto = {
   referrer?: string;
   timezoneLocalBrowser?: string;
   fingerPrint?: string;
-  crossSellId?: string;
+  crossSellId?: number;
   cloverOrder?: {
     tokenCard: string;
     orderSystemId: string;
