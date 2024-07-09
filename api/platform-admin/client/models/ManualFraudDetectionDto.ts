b837b51f01436407bb473c3029fdfa60ace01dd8
dropship-dev <tiena2cva@gmail.com>
2024-07-09 07:05:04 +0000

    Update from https://github.com/dropship-dev/backend/commit/a948e5072ad2711ada87a8246e6ac296f0c002be

--- api/platform-admin/client/models/ManualFraudDetectionDto.ts
+++ api/platform-admin/client/models/ManualFraudDetectionDto.ts
@@ -9,7 +9,7 @@ import type { FraudDetectionStatusType } from './FraudDetectionStatusType';
 export type ManualFraudDetectionDto = {
   fraudDetectionStatus: FraudDetectionStatusType;
   orderId?: string;
-  storeId?: string;
+  storeId: string;
   action: ActiveDetection;
 };
 
