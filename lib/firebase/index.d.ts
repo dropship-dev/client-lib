56c34d04e15811523bd232963facd75854def427
LynChiuz <LynChiuz@users.noreply.github.com>
2024-07-23 04:21:35 +0000

    Update build to output generated at c96a9fa

--- lib/firebase/index.d.ts
+++ lib/firebase/index.d.ts
@@ -2,6 +2,7 @@ export * from "firebase/auth";
 export declare function initializeFirebaseApp(): import("@firebase/app").FirebaseApp;
 export declare const app: import("@firebase/app").FirebaseApp;
 export declare const auth: import("@firebase/auth").Auth;
+export declare const messaging: import("@firebase/messaging").Messaging;
 export declare function getToken(): Promise<string>;
 export declare function getDeviceToken(): Promise<string>;
 export declare function isLoggedIn(): boolean;
