849dcdff800cac65d5aa8790ad44b80d572e0670
nvti <tiena2cva@gmail.com>
2024-07-25 02:11:16 +0000

    Update email-build to output generated at 0617f70

--- api/platform-admin/client/services/StatisticsService.js
+++ api/platform-admin/client/services/StatisticsService.js
@@ -10,7 +10,7 @@ class StatisticsService {
      * @returns any Ok
      * @throws ApiError
      */
-    getStatisticsBusinessPerformancesSummary({ startTime = '2023-01-01T00:00:00.000Z', endTime = '2024-07-25T02:03:43.936Z', }) {
+    getStatisticsBusinessPerformancesSummary({ startTime = '2023-01-01T00:00:00.000Z', endTime = '2024-07-25T02:10:16.316Z', }) {
         return this.httpRequest.request({
             method: 'GET',
             url: '/marketing/statistics/business-performances',
@@ -31,7 +31,7 @@ class StatisticsService {
      * @returns any Ok
      * @throws ApiError
      */
-    getStatisticsOverview({ startTime = '2023-01-01T00:00:00.000Z', endTime = '2024-07-25T02:03:43.951Z', timezone, }) {
+    getStatisticsOverview({ startTime = '2023-01-01T00:00:00.000Z', endTime = '2024-07-25T02:10:16.331Z', timezone, }) {
         return this.httpRequest.request({
             method: 'GET',
             url: '/marketing/statistics/automation-overview',
@@ -53,7 +53,7 @@ class StatisticsService {
      * @returns any Ok
      * @throws ApiError
      */
-    getStatisticsAutomationMetrics({ startTime = '2023-01-01T00:00:00.000Z', endTime = '2024-07-25T02:03:43.994Z', }) {
+    getStatisticsAutomationMetrics({ startTime = '2023-01-01T00:00:00.000Z', endTime = '2024-07-25T02:10:16.351Z', }) {
         return this.httpRequest.request({
             method: 'GET',
             url: '/marketing/statistics/automation-metrics',
@@ -74,7 +74,7 @@ class StatisticsService {
      * @returns any Ok
      * @throws ApiError
      */
-    getStatisticsPerforming({ startTime = '2023-01-01T00:00:00.000Z', endTime = '2024-07-25T02:03:44.008Z', }) {
+    getStatisticsPerforming({ startTime = '2023-01-01T00:00:00.000Z', endTime = '2024-07-25T02:10:16.359Z', }) {
         return this.httpRequest.request({
             method: 'GET',
             url: '/marketing/statistics/performing',
--- api/seller/client/services/MarketingEmailService.js
+++ api/seller/client/services/MarketingEmailService.js
@@ -10,7 +10,7 @@ class MarketingEmailService {
      * @returns any Ok
      * @throws ApiError
      */
-    getStatisticsStoreAbandonmentCheckout({ storeId, startTime = '2023-01-01T00:00:00.000Z', endTime = '2024-07-25T02:03:44.030Z', }) {
+    getStatisticsStoreAbandonmentCheckout({ storeId, startTime = '2023-01-01T00:00:00.000Z', endTime = '2024-07-25T02:10:16.379Z', }) {
         return this.httpRequest.request({
             method: 'GET',
             url: '/marketing/statistics/abandonments-checkout/store/{storeId}',
