d1b640e2efed2adadfb84019f76db1a797e36b4d
LynChiuz <100107550+LynChiuz@users.noreply.github.com>
2024-07-25 01:59:18 +0000

    Update build-pod to output generated at 9780016

--- lib/firebase/index.d.ts
+++ lib/firebase/index.d.ts
@@ -1,5 +1,8 @@
 export * from "firebase/auth";
+export declare const firebaseConfig: any;
+export declare const app: import("@firebase/app").FirebaseApp;
 export declare const auth: import("@firebase/auth").Auth;
+export declare const messaging: import("@firebase/messaging").Messaging;
 export declare function getToken(): Promise<string>;
 export declare function isLoggedIn(): boolean;
 export declare function getUser(): import("@firebase/auth").User | null;
