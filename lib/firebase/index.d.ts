860aaf249f4634d2af92cc2621367116281ac4c5
LynChiuz <100107550+LynChiuz@users.noreply.github.com>
2024-07-25 02:30:22 +0000

    Update build-pod to output generated at a70f893

--- lib/firebase/index.d.ts
+++ lib/firebase/index.d.ts
@@ -1,4 +1,5 @@
 export * from "firebase/auth";
+export declare const firebaseConfig: any;
 export declare const app: import("@firebase/app").FirebaseApp;
 export declare const auth: import("@firebase/auth").Auth;
 export declare function getToken(): Promise<string>;
