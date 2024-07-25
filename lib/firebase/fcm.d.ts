fcd17fc303357e2baf91d45ca689b5f6c8d82df4
LynChiuz <100107550+LynChiuz@users.noreply.github.com>
2024-07-25 07:25:38 +0000

    Update build-pod to output generated at 2d77352

---
+++ lib/firebase/fcm.d.ts
@@ -0,0 +1,3 @@
+export declare const firebaseApp: import("@firebase/app").FirebaseApp;
+export declare function initFirebaseApp(): import("@firebase/app").FirebaseApp;
+export declare const messaging: import("@firebase/messaging").Messaging;
