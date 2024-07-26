6183b7b548e243c91343b5cd25a3abafafefb0ab
LynChiuz <100107550+LynChiuz@users.noreply.github.com>
2024-07-26 02:45:57 +0000

    Update build-pod to output generated at 8814ad2

--- lib/firebase/index.d.ts
+++ lib/firebase/index.d.ts
@@ -7,7 +7,7 @@ export declare function testConsole(): Promise<void>;
 export declare function getDeviceToken(): Promise<string>;
 export declare function getToken(): Promise<string>;
 export declare function isLoggedIn(): boolean;
-export declare function getUser(): import("@firebase/auth").User | null;
+export declare function getUser(): import("@firebase/auth").User;
 export declare function signInWithGoogle(): Promise<import("@firebase/auth").User>;
 export declare function signInWithEmailPassword(email: string, password: string): Promise<import("@firebase/auth").User>;
 export declare function signUpWithEmailPassword(email: string, password: string): Promise<import("@firebase/auth").User>;
