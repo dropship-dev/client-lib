5510a62de6dee7d76e22ee711a25490733ad02be
dropship-dev <tiena2cva@gmail.com>
2024-07-26 04:05:29 +0000

    Update from https://github.com/dropship-dev/backend/commit/1500c525c89d17714762b1adbe7aa568a5733a46

--- api/seller/client/services/ProductService.ts
+++ api/seller/client/services/ProductService.ts
@@ -335,6 +335,41 @@ export class ProductService {
    * @returns any Ok
    * @throws ApiError
    */
+  public getCombosById({
+    storeId,
+    variantIds,
+  }: {
+    storeId: string,
+    variantIds: Array<number>,
+  }): CancelablePromise<Array<{
+    discount: any;
+    deleted: boolean;
+    isStock: boolean;
+    comboId: number;
+  }>> {
+    return this.httpRequest.request({
+      method: 'GET',
+      url: '/store/{storeId}/product/combos',
+      path: {
+        'storeId': storeId,
+      },
+      query: {
+        'variantIds': variantIds,
+      },
+      errors: {
+        400: `Bad request`,
+        401: `Invalid token`,
+        403: `Forbidden`,
+        404: `Not found`,
+        500: `Internal server error`,
+      },
+    });
+  }
+
+  /**
+   * @returns any Ok
+   * @throws ApiError
+   */
   public getVariantsById({
     storeId,
     variantIds,
