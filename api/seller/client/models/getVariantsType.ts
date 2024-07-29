bae2ee86dc8d47c1a1500a29254245d327ada615
dropship-dev <tiena2cva@gmail.com>
2024-07-29 10:38:32 +0000

    Update from https://github.com/dropship-dev/backend/commit/d02576862f8cc3e05abea055907eebeb2b567b6c

--- api/seller/client/models/getVariantsType.ts
+++ api/seller/client/models/getVariantsType.ts
@@ -3,22 +3,14 @@
 /* tslint:disable */
 /* eslint-disable */
 
-import type { CrossSell } from './CrossSell';
-import type { PlatformVariant } from './PlatformVariant';
-import type { Product } from './Product';
 import type { ProductVariant } from './ProductVariant';
 
 export type getVariantsType = (ProductVariant & {
-  Product: (Product & {
-    CrossSell: Array<CrossSell>;
-    Collection: Array<{
-      CrossSell: Array<CrossSell>;
-    }>;
-  });
-  PlatformVariant: PlatformVariant | null;
-} & {
   deleted: boolean;
   isStock: boolean;
+  Product: {
+    name: string;
+  };
   type: string;
 });
 
