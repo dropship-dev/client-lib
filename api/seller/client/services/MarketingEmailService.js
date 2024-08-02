eec2d7f51352ff19f726a8a7121c4c218a7aeb42
nvti <tiena2cva@gmail.com>
2024-08-02 08:52:15 +0000

    Update email-build to output generated at edc68f2

--- api/seller/client/services/MarketingEmailService.js
+++ api/seller/client/services/MarketingEmailService.js
@@ -10,7 +10,7 @@ class MarketingEmailService {
      * @returns any Ok
      * @throws ApiError
      */
-    getStatisticsStoreAbandonmentCheckout({ storeId, startTime = '2023-01-02T00:00:00.000Z', endTime = '2024-08-01T07:44:52.382Z', }) {
+    getStatisticsStoreAbandonmentCheckout({ storeId, startTime = '2023-01-02T00:00:00.000Z', endTime = '2024-08-02T08:51:02.716Z', }) {
         return this.httpRequest.request({
             method: 'GET',
             url: '/marketing/statistics/abandonments-checkout/store/{storeId}',
