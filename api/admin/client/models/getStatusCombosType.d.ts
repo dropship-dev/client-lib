7127610f4a9a56468d41e0bc008efddc61fd3743
nvti <tiena2cva@gmail.com>
2024-08-06 09:20:05 +0000

    Update build-production to output generated at 7c551e3

--- api/admin/client/models/getStatusCombosType.d.ts
+++ api/admin/client/models/getStatusCombosType.d.ts
@@ -3,6 +3,7 @@ export type getStatusCombosType = (VariantCombo & {
     deleted: boolean;
     isStock: boolean;
     Product: {
+        platformProductId: number | null;
         name: string;
     };
     type: string;
