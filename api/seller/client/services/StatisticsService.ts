fa81592cdf66ae8dc93ae619fe8f861fa4570e71
dropship-dev <tiena2cva@gmail.com>
2024-08-08 08:52:26 +0000

    Update from https://github.com/dropship-dev/backend/commit/b9e5ced0b9170db43678078070832b6efbf80e21

--- api/seller/client/services/StatisticsService.ts
+++ api/seller/client/services/StatisticsService.ts
@@ -16,7 +16,7 @@ export class StatisticsService {
   public getStatisticsStoreAbandonmentCheckout({
     storeId,
     startTime = '2023-01-02T00:00:00.000Z',
-    endTime = '2024-08-08T08:10:32.646Z',
+    endTime = '2024-08-08T08:51:45.366Z',
   }: {
     storeId: string,
     startTime?: string,
