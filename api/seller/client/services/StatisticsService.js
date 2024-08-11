c1d28ae59fe5aa3a4c429b6c9f819df8321c419c
nvti <tiena2cva@gmail.com>
2024-08-11 03:03:11 +0000

    Update build to output generated at 19d8f62

--- api/seller/client/services/StatisticsService.js
+++ api/seller/client/services/StatisticsService.js
@@ -10,7 +10,7 @@ class StatisticsService {
      * @returns any Ok
      * @throws ApiError
      */
-    getStatisticsStoreAbandonmentCheckout({ storeId, startTime = '2023-01-02T00:00:00.000Z', endTime = '2024-08-10T14:41:13.794Z', }) {
+    getStatisticsStoreAbandonmentCheckout({ storeId, startTime = '2023-01-02T00:00:00.000Z', endTime = '2024-08-11T03:02:08.761Z', }) {
         return this.httpRequest.request({
             method: 'GET',
             url: '/marketing/statistics/abandonments-checkout/store/{storeId}',
