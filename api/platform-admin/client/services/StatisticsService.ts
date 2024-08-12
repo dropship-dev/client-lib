70b64d7ef60a592226b1e70f1962b8c5763e0c62
dropship-dev <tiena2cva@gmail.com>
2024-08-12 02:38:07 +0000

    Update from https://github.com/dropship-dev/backend/commit/70ab2284a4774c5a5142d1be2ebe89ec9c36525e

--- api/platform-admin/client/services/StatisticsService.ts
+++ api/platform-admin/client/services/StatisticsService.ts
@@ -19,7 +19,7 @@ export class StatisticsService {
    */
   public getStatisticsBusinessPerformancesSummary({
     startTime = '2023-01-02T00:00:00.000Z',
-    endTime = '2024-08-11T10:44:28.908Z',
+    endTime = '2024-08-12T02:37:24.700Z',
   }: {
     startTime?: string,
     endTime?: string,
@@ -65,7 +65,7 @@ export class StatisticsService {
    */
   public getStatisticsOverview({
     startTime = '2023-01-02T00:00:00.000Z',
-    endTime = '2024-08-11T10:44:28.925Z',
+    endTime = '2024-08-12T02:37:24.717Z',
     timezone,
   }: {
     startTime?: string,
@@ -104,7 +104,7 @@ export class StatisticsService {
    */
   public getStatisticsAutomationMetrics({
     startTime = '2023-01-02T00:00:00.000Z',
-    endTime = '2024-08-11T10:44:28.963Z',
+    endTime = '2024-08-12T02:37:24.746Z',
   }: {
     startTime?: string,
     endTime?: string,
@@ -153,7 +153,7 @@ export class StatisticsService {
    */
   public getStatisticsPerforming({
     startTime = '2023-01-02T00:00:00.000Z',
-    endTime = '2024-08-11T10:44:28.993Z',
+    endTime = '2024-08-12T02:37:24.754Z',
   }: {
     startTime?: string,
     endTime?: string,
--- api/seller/client/services/MarketingEmailService.ts
+++ api/seller/client/services/MarketingEmailService.ts
@@ -28,7 +28,7 @@ export class MarketingEmailService {
   public getStatisticsStoreAbandonmentCheckout({
     storeId,
     startTime = '2023-01-02T00:00:00.000Z',
-    endTime = '2024-08-11T10:44:29.010Z',
+    endTime = '2024-08-12T02:37:24.762Z',
   }: {
     storeId: string,
     startTime?: string,
