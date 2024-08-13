049d9b4a1da4643ae5e738e355abb7fc14010b35
dropship-dev <tiena2cva@gmail.com>
2024-08-13 09:14:47 +0000

    Update from https://github.com/dropship-dev/backend/commit/dd98972b84d587b68709b5fd1fc07a6816eb748a

--- api/platform-admin/client/services/MarketingEmailService.ts
+++ api/platform-admin/client/services/MarketingEmailService.ts
@@ -19,7 +19,7 @@ export class MarketingEmailService {
    */
   public getStatisticsBusinessPerformancesSummary({
     startTime = '2023-01-02T00:00:00.000Z',
-    endTime = '2024-08-13T09:05:03.661Z',
+    endTime,
   }: {
     startTime?: string,
     endTime?: string,
@@ -65,7 +65,7 @@ export class MarketingEmailService {
    */
   public getStatisticsOverview({
     startTime = '2023-01-02T00:00:00.000Z',
-    endTime = '2024-08-13T09:05:03.676Z',
+    endTime,
     timezone,
   }: {
     startTime?: string,
@@ -104,7 +104,7 @@ export class MarketingEmailService {
    */
   public getStatisticsAutomationMetrics({
     startTime = '2023-01-02T00:00:00.000Z',
-    endTime = '2024-08-13T09:05:03.707Z',
+    endTime,
   }: {
     startTime?: string,
     endTime?: string,
@@ -153,7 +153,7 @@ export class MarketingEmailService {
    */
   public getStatisticsPerforming({
     startTime = '2023-01-02T00:00:00.000Z',
-    endTime = '2024-08-13T09:05:03.725Z',
+    endTime,
   }: {
     startTime?: string,
     endTime?: string,
