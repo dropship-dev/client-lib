5f954517e93158cde0d676f7e1a6f73e7c3bdf31
nvti <tiena2cva@gmail.com>
2024-08-23 02:21:49 +0000

    Update build to output generated at 2531c3b

--- api/seller/client/services/MarketingEmailService.d.ts
+++ api/seller/client/services/MarketingEmailService.d.ts
@@ -120,9 +120,9 @@ export declare class MarketingEmailService {
         comboItems: CreateComboItems;
         orderItems: CreateOrderItems;
         expiredAt: string;
+        discountShippingFee: number;
         subTotal: number;
         total: number;
-        shippingFee: number;
         email: string;
         EmailTrackings: Array<{
             action: EmailTrackingAction;
@@ -140,6 +140,7 @@ export declare class MarketingEmailService {
             price: number;
             photo: string;
         }>;
+        shippingFee: number;
     }>;
     /**
      * @returns void
