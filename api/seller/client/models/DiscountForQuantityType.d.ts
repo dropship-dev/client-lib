3bff54d96e50835eca04e1a0f06ec79193cc2b78
nvti <tiena2cva@gmail.com>
2024-08-27 03:12:43 +0000

    Update build to output generated at de5a676

--- api/seller/client/models/DiscountForQuantityType.d.ts
+++ api/seller/client/models/DiscountForQuantityType.d.ts
@@ -11,7 +11,6 @@ export type DiscountForQuantityType = {
     discounts: Array<{
         value: number;
         quantity: number;
-        productId: number;
         type: DiscountBoostSaleType;
     }>;
     timeLife: {
