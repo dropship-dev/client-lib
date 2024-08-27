ead220bda237f5464f7a838a0a55161c9b3d4a65
nvti <tiena2cva@gmail.com>
2024-08-27 03:10:57 +0000

    Update build to output generated at d5e5ee7

--- api/platform-admin/client/models/DiscountBoostSale.d.ts
+++ api/platform-admin/client/models/DiscountBoostSale.d.ts
@@ -1,5 +1,6 @@
 import type { DiscountBoostSaleType } from './DiscountBoostSaleType';
 export type DiscountBoostSale = Array<{
+    quantity?: number;
     value: number;
     productId: number;
     type: DiscountBoostSaleType;
--- api/seller/client/models/PrismaJson_DiscountBoostSale.d.ts
+++ api/seller/client/models/PrismaJson_DiscountBoostSale.d.ts
@@ -1,5 +1,6 @@
 import type { DiscountBoostSaleType } from './DiscountBoostSaleType';
 export type PrismaJson_DiscountBoostSale = Array<{
+    quantity?: number;
     value: number;
     productId: number;
     type: DiscountBoostSaleType;
