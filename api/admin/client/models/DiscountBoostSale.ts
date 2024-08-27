d5e5ee766bf709fc048488ec0985cf833873a8ad
dropship-dev <tiena2cva@gmail.com>
2024-08-27 03:10:16 +0000

    Update from https://github.com/dropship-dev/backend/commit/fbaea667cdb480c7028701291b9025f2b17cdd4f

--- api/admin/client/models/DiscountBoostSale.ts
+++ api/admin/client/models/DiscountBoostSale.ts
@@ -6,6 +6,7 @@
 import type { DiscountBoostSaleType } from './DiscountBoostSaleType';
 
 export type DiscountBoostSale = Array<{
+  quantity?: number;
   value: number;
   productId: number;
   type: DiscountBoostSaleType;
--- api/store/client/models/PrismaJson_DiscountBoostSale.ts
+++ api/store/client/models/PrismaJson_DiscountBoostSale.ts
@@ -6,6 +6,7 @@
 import type { DiscountBoostSaleType } from './DiscountBoostSaleType';
 
 export type PrismaJson_DiscountBoostSale = Array<{
+  quantity?: number;
   value: number;
   productId: number;
   type: DiscountBoostSaleType;
