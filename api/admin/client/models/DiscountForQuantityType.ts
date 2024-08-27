ac10e7b0e053f2dee2ec9c97aef3a710d939c482
dropship-dev <tiena2cva@gmail.com>
2024-08-27 02:15:38 +0000

    Update from https://github.com/dropship-dev/backend/commit/13ccc92b90cc98fede6c1263b035149291e7c574

--- api/admin/client/models/DiscountForQuantityType.ts
+++ api/admin/client/models/DiscountForQuantityType.ts
@@ -16,6 +16,7 @@ export type DiscountForQuantityType = {
   placementList: Array<PlacementBoostSaleEnum>;
   discounts: Array<{
     value: number;
+    quantity: number;
     productId: number;
     type: DiscountBoostSaleType;
   }>;
