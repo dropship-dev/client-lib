b8b8a36d19242477eb40b2458568b441ed088100
nvti <tiena2cva@gmail.com>
2024-08-28 10:22:17 +0000

    Update build to output generated at df757d9

--- api/admin/client/services/ProductService.js
+++ api/admin/client/services/ProductService.js
@@ -109,7 +109,7 @@ class ProductService {
      * @returns any Ok
      * @throws ApiError
      */
-    getStatusOrderItems({ storeId, variantIds, comboIds, crossSellId, }) {
+    getStatusOrderItems({ storeId, variantIds, comboIds, boostSaleIds, }) {
         return this.httpRequest.request({
             method: 'GET',
             url: '/store/{storeId}/product/getStatusOrderItems',
@@ -119,7 +119,7 @@ class ProductService {
             query: {
                 'variantIds': variantIds,
                 'comboIds': comboIds,
-                'crossSellId': crossSellId,
+                'boostSaleIds': boostSaleIds,
             },
             errors: {
                 400: `Bad request`,
