a90c5fc550ce67a6bcd23f49726cf9873611ea80
dropship-dev <tiena2cva@gmail.com>
2024-08-29 04:57:16 +0000

    Update from https://github.com/dropship-dev/backend/commit/30569229118ef95f1a3d34149fb464d787e29b93

--- api/store/client/models/getVariantsType.ts
+++ api/store/client/models/getVariantsType.ts
@@ -3,15 +3,17 @@
 /* tslint:disable */
 /* eslint-disable */
 
+import type { BoostSaleType } from './BoostSaleType';
 import type { ProductVariant } from './ProductVariant';
 
 export type getVariantsType = (ProductVariant & {
   deleted: boolean;
   isStock: boolean;
   Product?: {
-    CrossSell: {
+    boostSale: {
       id?: (number | string);
       status?: boolean;
+      type?: BoostSaleType | null;
     };
     platformProductId: number | null;
     name: string;
