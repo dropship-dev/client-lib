a99409bfb79edb310a864e6293ab1fc6ac6a9fb5
nvti <tiena2cva@gmail.com>
2024-08-29 09:06:51 +0000

    Update build-production to output generated at 0bc0c26

--- api/store/client/models/ProductPerformance.d.ts
+++ api/store/client/models/ProductPerformance.d.ts
@@ -1,4 +1,14 @@
-import type { ProductPerformanceResult } from './ProductPerformanceResult';
-export type ProductPerformance = (ProductPerformanceResult & {
+export type ProductPerformance = {
     photo: string;
-});
+    noOrders: number;
+    checkout: number;
+    addToCart: number;
+    viewProduct: number;
+    platformProductId?: number;
+    productId?: number;
+    CR: number;
+    AOV: number;
+    revenue: number;
+    name: string;
+    photos: Array<string>;
+};
