c83b8f0ae0e19f6b7be0c2a33a46ab94a938e6cf
dropship-dev <tiena2cva@gmail.com>
2024-09-04 10:18:45 +0000

    Update from https://github.com/dropship-dev/backend/commit/b367d7bb2dfa054633f1d614ce771539dbf0524a

--- api/seller/client/services/CollectionService.ts
+++ api/seller/client/services/CollectionService.ts
@@ -8,6 +8,7 @@ import type { CollectionStatus } from '../models/CollectionStatus';
 import type { CollectionType } from '../models/CollectionType';
 import type { CreateCollectionDto } from '../models/CreateCollectionDto';
 import type { operatorCondition } from '../models/operatorCondition';
+import type { Photos } from '../models/Photos';
 import type { Product } from '../models/Product';
 import type { UpdateCollectionDto } from '../models/UpdateCollectionDto';
 import type { UpdateCollectionStatusDto } from '../models/UpdateCollectionStatusDto';
@@ -77,10 +78,14 @@ export class CollectionService {
     total: number;
     data: Array<(Collection & {
       Product: Array<{
+        permalink: string;
+        photos: Photos;
+        name: string;
         ProductVariant: Array<{
           compareAtPrice: number;
           price: number;
         }>;
+        id: number;
       }>;
     })>;
   }> {
@@ -126,12 +131,16 @@ export class CollectionService {
     isEnableProduct?: boolean,
     deletedProduct?: boolean,
   }): CancelablePromise<(Collection & {
-    Product: Array<(Product & {
+    Product: Array<{
+      permalink: string;
+      photos: Photos;
+      name: string;
       ProductVariant: Array<{
         compareAtPrice: number;
         price: number;
       }>;
-    })>;
+      id: number;
+    }>;
     BoostSale: Array<BoostSale>;
   })> {
     return this.httpRequest.request({
