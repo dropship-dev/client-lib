29e22c56376c8feed99753c12bd0646ba8fee98f
dropship-dev <tiena2cva@gmail.com>
2024-09-05 02:25:52 +0000

    Update from https://github.com/dropship-dev/backend/commit/59739b813e36a24ab2aefa233234aa1b7f67970d

--- api/admin/client/models/CloneProductDto.ts
+++ api/admin/client/models/CloneProductDto.ts
@@ -6,13 +6,10 @@
 export type CloneProductDto = {
   combo?: boolean;
   discount?: boolean;
-  /**
-   * @deprecated
-   */
-  pixel?: boolean;
   fbPixel?: boolean;
   tiktokPixel?: boolean;
   googleAnalytic?: boolean;
+  googleTagManager?: boolean;
   shippingFee?: boolean;
   review?: boolean;
 };
