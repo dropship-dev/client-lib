54378293efd869cf31b9c02fa345b994baec1112
dropship-dev <tiena2cva@gmail.com>
2024-09-11 10:37:19 +0000

    Update from https://github.com/dropship-dev/backend/commit/dc5906e7e13092c85dadad587f47a8a715473674

--- api/admin/client/services/OrderService.ts
+++ api/admin/client/services/OrderService.ts
@@ -293,7 +293,10 @@ export class OrderService {
         tracking: string;
         orderId: string;
       }>;
-      Transaction: Array<Transaction>;
+      Transaction: Array<{
+        orderId: string;
+        status: TransactionStatus;
+      }>;
       Payment: {
         email: string;
         name: string;
