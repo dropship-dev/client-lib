c934367adf093622168a5c9fc2201938fd9e21ad
nvti <tiena2cva@gmail.com>
2024-09-12 04:33:31 +0000

    Update build to output generated at f94051d

--- api/seller/client/models/getVariantsType.d.ts
+++ api/seller/client/models/getVariantsType.d.ts
@@ -1,10 +1,12 @@
 import type { BoostSaleType } from './BoostSaleType';
+import type { PrismaJson_DiscountBoostSale } from './PrismaJson_DiscountBoostSale';
 import type { ProductVariant } from './ProductVariant';
 export type getVariantsType = (ProductVariant & {
     deleted: boolean;
     isStock: boolean;
     Product?: {
         boostSale: {
+            discount?: PrismaJson_DiscountBoostSale | null;
             id?: (number | string);
             status?: boolean;
             type?: BoostSaleType | null;
