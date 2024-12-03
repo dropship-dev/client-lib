ed32c5ea88f92a617f26a580b3e3318fd3324420
nvti <tiena2cva@gmail.com>
2024-12-03 08:29:53 +0000

    Update build to output generated at ed51bad

--- api/seller/client/services/DomainService.js
+++ api/seller/client/services/DomainService.js
@@ -138,6 +138,27 @@ class DomainService {
         });
     }
     /**
+     * @returns void
+     * @throws ApiError
+     */
+    disableDomainRenewal({ storeId, id, }) {
+        return this.httpRequest.request({
+            method: 'PATCH',
+            url: '/store/{storeId}/domain/disable-renewal/{id}',
+            path: {
+                'storeId': storeId,
+                'id': id,
+            },
+            errors: {
+                400: `Bad request`,
+                401: `Invalid token`,
+                403: `Forbidden`,
+                404: `Not found`,
+                500: `Internal server error`,
+            },
+        });
+    }
+    /**
      * @returns any Ok
      * @throws ApiError
      */
