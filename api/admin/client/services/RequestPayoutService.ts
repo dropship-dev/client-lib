ea8c6f92ec6164323cc9dc51316c2e8cd83fe7f5
dropship-dev <tiena2cva@gmail.com>
2024-12-04 14:54:34 +0000

    Update from https://github.com/dropship-dev/backend/commit/a18a3e034edf5aba59d1b07dad526b5d7ce8a77a

--- api/admin/client/services/RequestPayoutService.ts
+++ api/admin/client/services/RequestPayoutService.ts
@@ -7,10 +7,14 @@ import type { _36_Enums_PaymentMethodType } from '../models/_36_Enums_PaymentMet
 import type { _36_Enums_RequestPayoutStatus } from '../models/_36_Enums_RequestPayoutStatus';
 import type { _36_Enums_StoreStatus } from '../models/_36_Enums_StoreStatus';
 import type { _36_Enums_StoreType } from '../models/_36_Enums_StoreType';
+import type { BankAccount } from '../models/BankAccount';
 import type { CreateRequestPayoutDto } from '../models/CreateRequestPayoutDto';
+import type { PingPongAccount } from '../models/PingPongAccount';
 import type { PrismaJson_bankInfo } from '../models/PrismaJson_bankInfo';
 import type { PrismaJson_Photos } from '../models/PrismaJson_Photos';
 import type { PrismaJson_Timezone } from '../models/PrismaJson_Timezone';
+import type { RequestPayout } from '../models/RequestPayout';
+import type { Store } from '../models/Store';
 import type { UpdateRequestPayoutDto } from '../models/UpdateRequestPayoutDto';
 import type { CancelablePromise } from '../core/CancelablePromise';
 import type { BaseHttpRequest } from '../core/BaseHttpRequest';
@@ -237,80 +241,10 @@ export class RequestPayoutService {
   }: {
     storeId: string,
     id: string,
-  }): CancelablePromise<({
-    PingPongAccount: {
-      walletId: string;
-      isBlock: boolean;
-      isDefault: boolean;
-      email: string;
-      updatedAt: string;
-      createdAt: string;
-      isDeleted: boolean;
-      id: string;
-      currency: _36_Enums_CurrencyType;
-    };
-    BankAccount: {
-      walletId: string;
-      isBlock: boolean;
-      isDefault: boolean;
-      bank: PrismaJson_bankInfo;
-      accountHolder: string;
-      accountNumber: string;
-      updatedAt: string;
-      createdAt: string;
-      isDeleted: boolean;
-      id: string;
-      currency: _36_Enums_CurrencyType;
-    };
-    Store: {
-      stripeDefaultPaymentMethodId: string;
-      stripeCustomerId: string;
-      currencyId: number;
-      maxUsers: number;
-      balance: number;
-      referralCode: string;
-      defaultBankAccount: string;
-      shippingPolicy: string;
-      termsOfService: string;
-      privacyPolicy: string;
-      refundPolicy: string;
-      shippingFeeAdditional: number;
-      shippingFee: number;
-      primaryDomain: string;
-      subDomain: string;
-      pageName: string;
-      country: string;
-      zipCode: string;
-      city: string;
-      apartmentAddress: string;
-      address: string;
-      avatar: string;
-      timezone: PrismaJson_Timezone;
-      type: _36_Enums_StoreType;
-      status: _36_Enums_StoreStatus;
-      phone: string;
-      email: string;
-      name: string;
-      updatedAt: string;
-      createdAt: string;
-      fulfillmentAgencyId: number;
-      id: string;
-    };
-  } & {
-    photos: PrismaJson_Photos;
-    noteByAdmin: string;
-    noteBySeller: string;
-    convertCurrencyCode: string;
-    pingPongAccountId: string;
-    bankAccountId: string;
-    paymentMethod: _36_Enums_PaymentMethodType;
-    convertCurrencyAmount: number;
-    requestCurrencyAmount: number;
-    status: _36_Enums_RequestPayoutStatus;
-    updatedAt: string;
-    createdAt: string;
-    storeId: string;
-    id: string;
+  }): CancelablePromise<(RequestPayout & {
+    Store: Store | null;
+    PingPongAccount: PingPongAccount | null;
+    BankAccount: BankAccount | null;
   })> {
     return this.httpRequest.request({
       method: 'GET',
