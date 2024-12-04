b5598862f6a55810d5e634990e4cb45edd55f091
nvti <tiena2cva@gmail.com>
2024-12-04 12:04:06 +0000

    Update build to output generated at b6e62ef

--- api/marketing/client/services/UserService.d.ts
+++ api/marketing/client/services/UserService.d.ts
@@ -1,11 +1,7 @@
-import type { _36_Enums_CostCalculationMethod } from '../models/_36_Enums_CostCalculationMethod';
-import type { _36_Enums_FulfillmentAgencyRole } from '../models/_36_Enums_FulfillmentAgencyRole';
-import type { _36_Enums_FulfillmentAgencyStatus } from '../models/_36_Enums_FulfillmentAgencyStatus';
-import type { _36_Enums_FulfillmentAgencyType } from '../models/_36_Enums_FulfillmentAgencyType';
-import type { _36_Enums_UserRole } from '../models/_36_Enums_UserRole';
-import type { PrismaJson_FirebaseDeviceToken } from '../models/PrismaJson_FirebaseDeviceToken';
-import type { PrismaJson_Timezone } from '../models/PrismaJson_Timezone';
-import type { PrismaJson_UnavailableBalance } from '../models/PrismaJson_UnavailableBalance';
+import type { FulfillmentAgency } from '../models/FulfillmentAgency';
+import type { FulfillmentUser } from '../models/FulfillmentUser';
+import type { User } from '../models/User';
+import type { Wallet } from '../models/Wallet';
 import type { CancelablePromise } from '../core/CancelablePromise';
 import type { BaseHttpRequest } from '../core/BaseHttpRequest';
 export declare class UserService {
@@ -15,61 +11,11 @@ export declare class UserService {
      * @returns any Ok
      * @throws ApiError
      */
-    getUser(): CancelablePromise<({
-        FulfillmentUser: Array<({
-            FulfillmentAgency: ({
-                Wallet: Array<{
-                    updatedAt: string;
-                    createdAt: string;
-                    isDeleted: boolean;
-                    storeId: string;
-                    fulfillmentAgencyId: number;
-                    balanceUnavailable: PrismaJson_UnavailableBalance;
-                    payoutAmount: number;
-                    holdAmount: number;
-                    balanceDebt: number;
-                    balanceAvailable: number;
-                    balanceAmount: number;
-                    walletName: string;
-                    id: string;
-                }>;
-            } & {
-                timezone: PrismaJson_Timezone;
-                type: _36_Enums_FulfillmentAgencyType;
-                costCalculationMethod: _36_Enums_CostCalculationMethod;
-                executionTime: string;
-                status: _36_Enums_FulfillmentAgencyStatus;
-                phone: string;
-                email: string;
-                name: string;
-                updatedAt: string;
-                createdAt: string;
-                id: number;
+    getUser(): CancelablePromise<(User & {
+        FulfillmentUser: Array<(FulfillmentUser & {
+            FulfillmentAgency: (FulfillmentAgency & {
+                Wallet: Array<Wallet>;
             });
-        } & {
-            isOwner: boolean;
-            userId: string;
-            role: _36_Enums_FulfillmentAgencyRole;
-            updatedAt: string;
-            createdAt: string;
-            fulfillmentAgencyId: number;
-            id: number;
         })>;
-    } & {
-        subscriptionId: string;
-        firebaseDeviceToken: PrismaJson_FirebaseDeviceToken;
-        firstLogin: boolean;
-        maxStaffStores: number;
-        maxOwnedStores: number;
-        shortId: string;
-        role: _36_Enums_UserRole;
-        country: string;
-        avatar: string;
-        phone: string;
-        email: string;
-        name: string;
-        updatedAt: string;
-        createdAt: string;
-        id: string;
     })>;
 }
