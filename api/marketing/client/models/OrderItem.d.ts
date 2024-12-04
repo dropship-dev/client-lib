688fdfacb988e964edcca10df55a2c8f5aeca30b
nvti <tiena2cva@gmail.com>
2024-12-04 13:24:29 +0000

    Update build to output generated at ce58209

---
+++ api/marketing/client/models/OrderItem.d.ts
@@ -0,0 +1,5 @@
+import type { DefaultSelection_Prisma__36_OrderItemPayload_ } from './DefaultSelection_Prisma__36_OrderItemPayload_';
+/**
+ * Model OrderItem
+ */
+export type OrderItem = DefaultSelection_Prisma__36_OrderItemPayload_;
---
+++ api/platform-admin/client/models/Order.d.ts
@@ -0,0 +1,5 @@
+import type { DefaultSelection_Prisma__36_OrderPayload_ } from './DefaultSelection_Prisma__36_OrderPayload_';
+/**
+ * Model Order
+ */
+export type Order = DefaultSelection_Prisma__36_OrderPayload_;
