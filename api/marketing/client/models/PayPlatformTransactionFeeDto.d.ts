9245629ccddc8afb5d3cfb166564bb849c052d15
nvti <tiena2cva@gmail.com>
2024-12-25 03:50:08 +0000

    Update build to output generated at 7a917dd

--- api/marketing/client/models/PayPlatformTransactionFeeDto.d.ts
+++ api/marketing/client/models/PayPlatformTransactionFeeDto.d.ts
@@ -1,10 +1,9 @@
 import type { _36_Enums_SubscriptionInterval } from './_36_Enums_SubscriptionInterval';
-import type { StripeCardPaymentInfo } from './StripeCardPaymentInfo';
 export type PayPlatformTransactionFeeDto = {
     platformFee: number;
     subscriptionPlan?: {
         interval: _36_Enums_SubscriptionInterval;
         id: number;
-    };
-    paymentInfo: StripeCardPaymentInfo;
+    } | null;
+    paymentMethodId: string;
 };
