7a917dd108151ff4df980bb8d1fc2923b98a8eff
dropship-dev <tiena2cva@gmail.com>
2024-12-25 03:49:45 +0000

    Update from https://github.com/dropship-dev/backend/commit/58be3087cdbf18b6707f017dc1fd1b80b445291b

--- api/platform-admin/client/models/PayPlatformTransactionFeeDto.ts
+++ api/platform-admin/client/models/PayPlatformTransactionFeeDto.ts
@@ -3,13 +3,12 @@
 /* tslint:disable */
 /* eslint-disable */
 import type { _36_Enums_SubscriptionInterval } from './_36_Enums_SubscriptionInterval';
-import type { StripeCardPaymentInfo } from './StripeCardPaymentInfo';
 export type PayPlatformTransactionFeeDto = {
   platformFee: number;
   subscriptionPlan?: {
     interval: _36_Enums_SubscriptionInterval;
     id: number;
-  };
-  paymentInfo: StripeCardPaymentInfo;
+  } | null;
+  paymentMethodId: string;
 };
 
