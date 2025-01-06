da6d687907e7d6e7cc5468a60a0a9a0de36d687d
dropship-dev <tiena2cva@gmail.com>
2025-01-06 06:12:12 +0000

    Update from https://github.com/dropship-dev/backend/commit/8cb9b502d62b34c1ec4bede5c70ab1d0ccaade89

--- api/admin/client/models/CreateFulfillmentAgencyDto.ts
+++ api/admin/client/models/CreateFulfillmentAgencyDto.ts
@@ -3,10 +3,12 @@
 /* tslint:disable */
 /* eslint-disable */
 import type { _36_Enums_FulfillmentAgencyType } from './_36_Enums_FulfillmentAgencyType';
+import type { _36_Enums_FulfillmentPlatform } from './_36_Enums_FulfillmentPlatform';
 export type CreateFulfillmentAgencyDto = {
   name: string;
   email: string;
   phone?: string;
   type?: _36_Enums_FulfillmentAgencyType;
+  integrateWithFulfillmentPlatform?: _36_Enums_FulfillmentPlatform | null;
 };
 
