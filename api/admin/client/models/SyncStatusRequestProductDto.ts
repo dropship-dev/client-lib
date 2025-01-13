759925dc0136b7b5fd0a7ab428a28410d8da0e19
dropship-dev <tiena2cva@gmail.com>
2025-01-13 08:22:55 +0000

    Update from https://github.com/dropship-dev/backend/commit/6efa141add0b8b22b840e9a30480bd5142f61ba9

--- api/admin/client/models/SyncStatusRequestProductDto.ts
+++ api/admin/client/models/SyncStatusRequestProductDto.ts
@@ -4,7 +4,7 @@
 /* eslint-disable */
 import type { _36_Enums_RequestSourcingStatus } from './_36_Enums_RequestSourcingStatus';
 export type SyncStatusRequestProductDto = {
-  requestProductId: Array<number>;
+  requestProductIds: Array<number>;
   status: _36_Enums_RequestSourcingStatus;
   platformProductId?: number;
 };
