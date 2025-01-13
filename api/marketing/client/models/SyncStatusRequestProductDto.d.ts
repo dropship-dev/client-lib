de80fca0d4c44bf585c450c86957db65dbbf7abb
nvti <tiena2cva@gmail.com>
2025-01-13 08:23:22 +0000

    Update build to output generated at 759925d

--- api/marketing/client/models/SyncStatusRequestProductDto.d.ts
+++ api/marketing/client/models/SyncStatusRequestProductDto.d.ts
@@ -1,6 +1,6 @@
 import type { _36_Enums_RequestSourcingStatus } from './_36_Enums_RequestSourcingStatus';
 export type SyncStatusRequestProductDto = {
-    requestProductId: Array<number>;
+    requestProductIds: Array<number>;
     status: _36_Enums_RequestSourcingStatus;
     platformProductId?: number;
 };
