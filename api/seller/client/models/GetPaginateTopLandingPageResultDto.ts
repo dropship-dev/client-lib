7ec6874f8cf23f5427311abad979944d56a50838
dropship-dev <tiena2cva@gmail.com>
2025-02-05 09:58:13 +0000

    Update from https://github.com/dropship-dev/backend/commit/75acb946163a5a64d41caea46a55a1eb04fabc99

---
+++ api/seller/client/models/GetPaginateTopLandingPageResultDto.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { GetTopLandingPageResultDto } from './GetTopLandingPageResultDto';
+export type GetPaginateTopLandingPageResultDto = {
+  data: Array<GetTopLandingPageResultDto>;
+  preCursor: boolean;
+  nextCursor: string | null;
+};
+
