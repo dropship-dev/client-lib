cc5e68fc45355f5d2ac814c87fd39737e811695f
dropship-dev <tiena2cva@gmail.com>
2025-02-14 09:38:14 +0000

    Update from https://github.com/dropship-dev/backend/commit/45ce6931191529980405abd5bec600fcfd782e08

--- api/seller/client/services/SubscriptionService.ts
+++ api/seller/client/services/SubscriptionService.ts
@@ -108,6 +108,7 @@ export class SubscriptionService {
     stripeSubscriptionId: string;
     upgradeSubscriptionPlan: PrismaJson_UpgradeSubscriptionPlan;
     subscriptionPlanId: number;
+    passRevenueCeiling: boolean;
     failedPTFCaptureTimes: number;
     lastPlatformFeeChargeAt: string;
     currentPlatformTransactionFee: number;
