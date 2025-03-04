1e17f1ad981fd8db68aa13e79f6c7a6ce50f1c9f
nvti <tiena2cva@gmail.com>
2025-03-04 09:13:02 +0000

    Update build to output generated at 2ef9608

--- api/platform-admin/client/models/_36_Enums_UserRole.d.ts
+++ api/platform-admin/client/models/_36_Enums_UserRole.d.ts
@@ -2,5 +2,6 @@ export declare enum _36_Enums_UserRole {
     PLATFORM_ADMIN = "PLATFORM_ADMIN",
     ADMIN = "ADMIN",
     SELLER = "SELLER",
-    MARKETING = "MARKETING"
+    MARKETING = "MARKETING",
+    SALE = "SALE"
 }
