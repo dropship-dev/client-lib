1a69471656b2eed707aeb279041abaf36aa795b5
dropship-dev <tiena2cva@gmail.com>
2025-03-10 09:34:35 +0000

    Update from https://github.com/dropship-dev/backend/commit/6f8f5a48b3ceae863ca9a1e95c61afddf7eebef2

--- api/admin/client/models/Pick_Payment_Exclude_keyofPayment_secretKey_or_token_or_tokenExpiredAt_or_deleted_or_userId_or_isPlatform_or_gatewayUrl_or_partnerId_or_salt_or_PaymentIntegrationType__.ts
+++ api/admin/client/models/Pick_Payment_Exclude_keyofPayment_secretKey_or_token_or_tokenExpiredAt_or_deleted_or_userId_or_isPlatform_or_gatewayUrl_or_partnerId_or_salt_or_PaymentIntegrationType__.ts
@@ -3,12 +3,11 @@
 /* tslint:disable */
 /* eslint-disable */
 import type { _36_Enums_EnvironmentType } from './_36_Enums_EnvironmentType';
-import type { _36_Enums_PaymentIntegrationType } from './_36_Enums_PaymentIntegrationType';
 import type { _36_Enums_PaymentType } from './_36_Enums_PaymentType';
 /**
  * From T, pick a set of properties whose keys are in the union K
  */
-export type Pick_Payment_Exclude_keyofPayment_secretKey_or_token_or_tokenExpiredAt_or_deleted_or_userId_or_isPlatform_or_gatewayUrl_or_partnerId_or_salt__ = {
+export type Pick_Payment_Exclude_keyofPayment_secretKey_or_token_or_tokenExpiredAt_or_deleted_or_userId_or_isPlatform_or_gatewayUrl_or_partnerId_or_salt_or_PaymentIntegrationType__ = {
   id: number;
   fulfillmentAgencyId: number;
   createdAt: string;
@@ -25,6 +24,5 @@ export type Pick_Payment_Exclude_keyofPayment_secretKey_or_token_or_tokenExpired
   BNcode: string;
   UIVersion: number;
   environment: _36_Enums_EnvironmentType;
-  PaymentIntegrationType: _36_Enums_PaymentIntegrationType;
 };
 
