19f891bb587006def49f5462b063ad9de1c04a53
nvti <tiena2cva@gmail.com>
2025-03-19 03:10:48 +0000

    Update build to output generated at d784eaa

---
+++ api/platform-admin/client/models/PrismaJson_Permissions.d.ts
@@ -0,0 +1,3 @@
+import type { Permission } from './Permission';
+import type { SuperAdminPermission } from './SuperAdminPermission';
+export type PrismaJson_Permissions = Array<(Permission | SuperAdminPermission)>;
