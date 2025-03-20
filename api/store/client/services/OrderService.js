4479e4c9ea9e2ae5b275a70213ab3e84314a7162
nvti <tiena2cva@gmail.com>
2025-03-20 04:42:05 +0000

    Update build-production to output generated at a0c09c9

--- api/store/client/services/OrderService.js
+++ api/store/client/services/OrderService.js
@@ -145,5 +145,26 @@ class OrderService {
             },
         });
     }
+    /**
+     * @returns any Ok
+     * @throws ApiError
+     */
+    getRevenueOrder({ orderId, storeId, }) {
+        return this.httpRequest.request({
+            method: 'GET',
+            url: '/store/{storeId}/order/{orderId}/revenue',
+            path: {
+                'orderId': orderId,
+                'storeId': storeId,
+            },
+            errors: {
+                400: `Bad request`,
+                401: `Invalid token`,
+                403: `Forbidden`,
+                404: `Not found`,
+                500: `Internal server error`,
+            },
+        });
+    }
 }
 exports.OrderService = OrderService;
