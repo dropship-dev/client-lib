46c1de2c10ec2e0f74538dbbc288be3f15e0ad69
nvti <tiena2cva@gmail.com>
2025-03-21 09:39:25 +0000

    Update build to output generated at 47f0640

---
+++ api/store/client/models/PaymentTypeCustom.d.ts
@@ -0,0 +1,20 @@
+import type { _36_Enums_EnvironmentType } from './_36_Enums_EnvironmentType';
+import type { _36_Enums_PaymentType } from './_36_Enums_PaymentType';
+export type PaymentTypeCustom = {
+    id: number;
+    fulfillmentAgencyId: number;
+    createdAt: string;
+    updatedAt: string;
+    name: string;
+    email: string;
+    type: _36_Enums_PaymentType;
+    merchantId: string;
+    companyName: string;
+    companyPhone: string;
+    companyAddress: string;
+    isShowCompanyAddress: boolean;
+    publishableKey: string;
+    BNcode: string;
+    UIVersion: number;
+    environment: _36_Enums_EnvironmentType;
+};
