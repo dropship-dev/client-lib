b691e9f9aa77cc60d10510b9965d0702e44905af
nvti <tiena2cva@gmail.com>
2025-04-22 04:04:39 +0000

    Update build to output generated at 49b9640

--- api/sale/client/models/_36_Enums_AsyncTaskType.js
+++ api/sale/client/models/_36_Enums_AsyncTaskType.js
@@ -7,6 +7,7 @@ exports._36_Enums_AsyncTaskType = void 0;
 /* eslint-disable */
 var _36_Enums_AsyncTaskType;
 (function (_36_Enums_AsyncTaskType) {
+    _36_Enums_AsyncTaskType["EXPORT_PAYOUT_REQUEST"] = "EXPORT_PAYOUT_REQUEST";
     _36_Enums_AsyncTaskType["EXPORT_ORDER"] = "EXPORT_ORDER";
     _36_Enums_AsyncTaskType["EXPORT_ACCOUNTANCY"] = "EXPORT_ACCOUNTANCY";
     _36_Enums_AsyncTaskType["GENERATE_PNG"] = "GENERATE_PNG";
