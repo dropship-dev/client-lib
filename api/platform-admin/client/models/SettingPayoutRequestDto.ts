463983be673af14f420268d723539fbda4b0db88
dropship-dev <tiena2cva@gmail.com>
2025-04-28 08:37:44 +0000

    Update from https://github.com/dropship-dev/backend/commit/f8560deeb7047d3d5beaa9edb55f2e198665510a

--- api/platform-admin/client/models/SettingPayoutRequestDto.ts
+++ api/platform-admin/client/models/SettingPayoutRequestDto.ts
@@ -5,6 +5,6 @@
 import type { PrismaJson_SettingPayoutRequestDay } from './PrismaJson_SettingPayoutRequestDay';
 export type SettingPayoutRequestDto = {
   receiverRequestTime: Array<string>;
-  receiverRequestDay: Array<PrismaJson_SettingPayoutRequestDay>;
+  receiverRequestDay: PrismaJson_SettingPayoutRequestDay;
 };
 
