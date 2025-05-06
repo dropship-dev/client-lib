cc6a83372778af3554155721a8dfd9a5c918cf7f
dropship-dev <tiena2cva@gmail.com>
2025-05-06 05:40:45 +0000

    Update from https://github.com/dropship-dev/backend/commit/9f926ecafd8fc0ff097296df5a1f25bec8f815a6

---
+++ api/admin/client/models/StoreLogType.ts
@@ -0,0 +1,6 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { _36_Enums_StoreLogType } from './_36_Enums_StoreLogType';
+export type StoreLogType = _36_Enums_StoreLogType;
---
+++ api/marketing/client/models/CreateLogDto.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { _36_Enums_StoreLogType } from './_36_Enums_StoreLogType';
+import type { Record_string_any_ } from './Record_string_any_';
+export type CreateLogDto = {
+  type: _36_Enums_StoreLogType;
+  message: string;
+  data: Record_string_any_;
+};
+
