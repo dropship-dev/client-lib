e9dea502ce99ad0ff8120e4150996352b37b7b39
nvti <tiena2cva@gmail.com>
2025-05-14 06:55:38 +0000

    Update build to output generated at ab98fa4

--- api/admin/client/models/GetStoreReportResultDto.d.ts
+++ api/admin/client/models/GetStoreReportResultDto.d.ts
@@ -1,7 +1,7 @@
 export type GetStoreReportResultDto = {
     data: Array<{
         total: number;
-        profit: number;
+        originalProfit: number | null;
         createdAt: string;
         id: string;
         Store: {
