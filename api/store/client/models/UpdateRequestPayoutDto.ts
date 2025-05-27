c3a8b350e7c82cf89846d04efe1e9c8b2581b1c8
dropship-dev <tiena2cva@gmail.com>
2025-05-27 02:25:21 +0000

    Update from https://github.com/dropship-dev/backend/commit/25a4eea938845272e6d854c814e297412363c030

--- api/store/client/models/UpdateRequestPayoutDto.ts
+++ api/store/client/models/UpdateRequestPayoutDto.ts
@@ -5,7 +5,7 @@
 import type { PhotoDto } from './PhotoDto';
 import type { statusRequestPayout } from './statusRequestPayout';
 export type UpdateRequestPayoutDto = {
-  photos: Array<PhotoDto>;
+  photos?: Array<PhotoDto>;
   notes?: string;
   status?: statusRequestPayout;
 };
