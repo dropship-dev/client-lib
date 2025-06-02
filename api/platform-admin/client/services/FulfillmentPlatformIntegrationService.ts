323d10accbe37cbb23a719f99d4c64364d597d2b
dropship-dev <tiena2cva@gmail.com>
2025-06-02 09:08:23 +0000

    Update from https://github.com/dropship-dev/backend/commit/0b6dd233f0d5b3bed0d1ea7bc44de70d35842edb

--- api/platform-admin/client/services/FulfillmentPlatformIntegrationService.ts
+++ api/platform-admin/client/services/FulfillmentPlatformIntegrationService.ts
@@ -2,6 +2,8 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
+import type { PrismaJson_Photos } from '../models/PrismaJson_Photos';
+import type { PrismaJson_VariantOptions } from '../models/PrismaJson_VariantOptions';
 import type { SearchBettaSupSellersResponse } from '../models/SearchBettaSupSellersResponse';
 import type { CancelablePromise } from '../core/CancelablePromise';
 import type { BaseHttpRequest } from '../core/BaseHttpRequest';
@@ -31,6 +33,39 @@ export class FulfillmentPlatformIntegrationService {
       errors: {
         400: `Bad request`,
         401: `Invalid token`,
+        403: `Forbidden`,
+        404: `Not found`,
+        500: `Internal server error`,
+      },
+    });
+  }
+  /**
+   * @returns any Ok
+   * @throws ApiError
+   */
+  public getProductDataFromProductUrl({
+    xBettamaxApiKey,
+    url,
+  }: {
+    xBettamaxApiKey: string,
+    url: string,
+  }): CancelablePromise<{
+    variantOptions: PrismaJson_VariantOptions;
+    description: string;
+    photos: PrismaJson_Photos;
+    name: string;
+  }> {
+    return this.httpRequest.request({
+      method: 'GET',
+      url: '/fulfillment-platform-integration/product-data-by-permalink',
+      headers: {
+        'x-bettamax-api-key': xBettamaxApiKey,
+      },
+      query: {
+        'url': url,
+      },
+      errors: {
+        400: `Bad request`,
         403: `Forbidden`,
         404: `Not found`,
         500: `Internal server error`,
