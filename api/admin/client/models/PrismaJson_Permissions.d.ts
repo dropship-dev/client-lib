720a07651a3ccfd6dbe015ec21a54c88d5e12286
nvti <tiena2cva@gmail.com>
2025-06-23 02:17:36 +0000

    Update build to output generated at d401fce

--- api/admin/client/models/PrismaJson_Permissions.d.ts
+++ api/admin/client/models/PrismaJson_Permissions.d.ts
@@ -1,4 +1,5 @@
+import type { FulfillmentPermissions } from './FulfillmentPermissions';
 import type { Permission } from './Permission';
 import type { SellerPermission } from './SellerPermission';
 import type { SuperAdminPermission } from './SuperAdminPermission';
-export type PrismaJson_Permissions = Array<(Permission | SuperAdminPermission | SellerPermission)>;
+export type PrismaJson_Permissions = Array<(Permission | SuperAdminPermission | SellerPermission | FulfillmentPermissions)>;
