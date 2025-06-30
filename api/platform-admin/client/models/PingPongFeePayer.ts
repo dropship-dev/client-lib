bfb0dfb659060e7e04faf79e75ef903968b87290
dropship-dev <tiena2cva@gmail.com>
2025-06-30 09:38:06 +0000

    Update from https://github.com/dropship-dev/backend/commit/4332a64c963e6d96f853b563a87a8d2649cbb162

--- api/platform-admin/client/models/PingPongFeePayer.ts
+++
@@ -1,8 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export enum PingPongFeePayer {
-  TARGET_PAY_FEE = 'TARGET_PAY_FEE',
-  ORIGIN_PAY_FEE = 'ORIGIN_PAY_FEE',
-}
