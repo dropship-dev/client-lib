326d0e17f356917183da1db943f2618fbe99ac3a
nvti <tiena2cva@gmail.com>
2025-07-30 09:24:11 +0000

    Update build to output generated at 747aeee

--- api/platform-admin/client/models/TaxProviderPaymentItem.d.ts
+++ api/platform-admin/client/models/TaxProviderPaymentItem.d.ts
@@ -1,5 +1,6 @@
 import type { _36_Enums_PaymentType } from './_36_Enums_PaymentType';
 export type TaxProviderPaymentItem = {
+    isPlatform: boolean;
     name: string;
     type: _36_Enums_PaymentType;
     id: number;
