747aeee5d6edd768a57589e0aa0c9e89fb4f58a1
dropship-dev <tiena2cva@gmail.com>
2025-07-30 09:23:41 +0000

    Update from https://github.com/dropship-dev/backend/commit/cc462defff4418b02b68ca593ea6eb843711a7ad

--- api/sale/client/models/TaxProviderPaymentItem.ts
+++ api/sale/client/models/TaxProviderPaymentItem.ts
@@ -4,6 +4,7 @@
 /* eslint-disable */
 import type { _36_Enums_PaymentType } from './_36_Enums_PaymentType';
 export type TaxProviderPaymentItem = {
+  isPlatform: boolean;
   name: string;
   type: _36_Enums_PaymentType;
   id: number;
