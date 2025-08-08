ef4e60688f6c03c5c9e25cb42df4816b3b843d10
nvti <tiena2cva@gmail.com>
2025-08-08 06:28:39 +0000

    Update build to output generated at 1327369

--- api/store/client/models/CreateRequestPayoutDto.d.ts
+++ api/store/client/models/CreateRequestPayoutDto.d.ts
@@ -4,8 +4,14 @@ export type CreateRequestPayoutDto = {
     otp: string;
     amount: number;
     currencyCode: CurrencyCode;
+    /**
+     * @deprecated
+     */
     amountConvert: number;
     type: Exclude_PaymentMethodType_CASH_or_BANK_TRANSFER_;
+    /**
+     * @deprecated
+     */
     bankId?: string;
     pingPongId: string;
     notes?: string;
