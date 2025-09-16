47afaec2ca20fd24ae8c5b6641899788c25e1c31
nvti <tiena2cva@gmail.com>
2025-09-16 04:06:52 +0000

    Update build-ads to output generated at 3bbc3ab

--- api/store/client/core/AxiosHttpRequest.js
+++ api/store/client/core/AxiosHttpRequest.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.FetchHttpRequest = void 0;
+exports.AxiosHttpRequest = void 0;
 const BaseHttpRequest_1 = require("./BaseHttpRequest");
 const request_1 = require("./request");
-class FetchHttpRequest extends BaseHttpRequest_1.BaseHttpRequest {
+class AxiosHttpRequest extends BaseHttpRequest_1.BaseHttpRequest {
     constructor(config) {
         super(config);
     }
@@ -17,4 +17,4 @@ class FetchHttpRequest extends BaseHttpRequest_1.BaseHttpRequest {
         return (0, request_1.request)(this.config, options);
     }
 }
-exports.FetchHttpRequest = FetchHttpRequest;
+exports.AxiosHttpRequest = AxiosHttpRequest;
