7c59e684fe1f39a8207f733543d037c2c4273e4e
dropship-dev <tiena2cva@gmail.com>
2025-09-16 08:40:13 +0000

    Update from https://github.com/dropship-dev/backend/commit/ff30dfbbedc565b1dded60d17cfc9e9a96421c29

--- api/store/index.ts
+++ api/store/index.ts
@@ -170,44 +170,6 @@ declare global {
   var isFetchIntercepted: boolean;
 }
 
-class ClientApi extends _ClientApi {
-  public async uploadFile(file: File): Promise<string> {
-    let result = {
-      signedUrl: "",
-      url: "",
-    };
-    try {
-      result = await this.upload.createUpload({
-        requestBody: {
-          contentLength: file.size,
-          fileName: file.name,
-        },
-      });
-    } catch (e) {
-      if (e instanceof ApiError) {
-        if ("details" in e.body && e.body.details === "File too large") {
-          throw new Error("File too large");
-        }
-      }
-
-      throw e;
-    }
-
-    const body = new Blob([file], { type: file.type });
-    await this.request.request({
-      method: "PUT",
-      url: result.signedUrl,
-      body: body,
-      headers: {
-        "Content-Type": file.type,
-        "Content-Length": file.size,
-      }
-    })
-
-    return result.url;
-  }
-}
-
 export const api = new ClientApi({
   BASE: OpenAPI.BASE,
   TOKEN: OpenAPI.TOKEN,
