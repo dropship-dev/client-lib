7fafd181ac3a707e23ac2dfbcd45085b379b913e
nvti <tiena2cva@gmail.com>
2025-10-30 08:31:52 +0000

    Update build-production to output generated at 798d526

--- api/seller/client/models/UpdateGoogleAdWordsDto.d.ts
+++ api/seller/client/models/UpdateGoogleAdWordsDto.d.ts
@@ -1,3 +1,5 @@
+import type { _36_Enums_GoogleAdWordsTrackingEventType } from './_36_Enums_GoogleAdWordsTrackingEventType';
 export type UpdateGoogleAdWordsDto = {
     productIds: Array<number>;
+    eventType?: _36_Enums_GoogleAdWordsTrackingEventType;
 };
