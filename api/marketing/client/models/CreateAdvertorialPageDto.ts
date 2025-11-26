d63d3171c63f4f34fc6a9b97a1702872ff174bc2
dropship-dev <tiena2cva@gmail.com>
2025-11-26 06:26:49 +0000

    Update from https://github.com/dropship-dev/backend/commit/0676b484b697b7bd1410173614683fe71ac5a705

--- api/marketing/client/models/CreateAdvertorialPageDto.ts
+++ api/marketing/client/models/CreateAdvertorialPageDto.ts
@@ -6,7 +6,7 @@ export type CreateAdvertorialPageDto = {
   title: string;
   pageBuilderContent: string;
   pageContent?: string;
-  seoTitle: string;
+  seoTitle?: string;
   permalink: string;
   metaDescription?: string;
   isVisible: boolean;
